4b3bcaa531d3f973c3ea2c9508c2029a1bafb9b5
An <thanhan7501@gmail.com>
2024-03-28 20:55:01 +0700

    fix upload file

--- apps/cms/src/controllers/fileUpload.controller.ts
+++ apps/cms/src/controllers/fileUpload.controller.ts
@@ -1,10 +1,11 @@
-import { BlobServiceClient, StorageSharedKeyCredential } from "@azure/storage-blob"
-import fileUploadModel from "../models/fileUpload.model";
-import multer from "multer";
-const account = process.env.account
-const accountKey = process.env.accountKey
+import {
+  BlobServiceClient,
+  StorageSharedKeyCredential,
+} from '@azure/storage-blob';
+import fileUploadModel from '../models/fileUpload.model';
 
-const upload = multer();
+const account = process.env.account;
+const accountKey = process.env.accountKey;
 
 const sharedKeyCredential = new StorageSharedKeyCredential(account, accountKey);
 const blobServiceClient = new BlobServiceClient(
@@ -12,53 +13,50 @@ const blobServiceClient = new BlobServiceClient(
   sharedKeyCredential
 );
 
-const containerName = process.env.containerName
+const containerName = process.env.containerName;
 
 export const createBlob = async (req, res, next) => {
-    try {
+  try {
     const containerClient = blobServiceClient.getContainerClient(containerName);
-  
-    const content = "Hello world!";
-    const blobName = "newblob" + new Date().getTime();
+
+    const content = 'Hello world!';
+    const blobName = 'newblob' + new Date().getTime();
     const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-    const uploadBlobResponse = await blockBlobClient.upload(content, content.length);
-    console.log(`Upload block blob ${blobName} successfully`, uploadBlobResponse.requestId);
-}
-  catch(error)
-  {
+    const uploadBlobResponse = await blockBlobClient.upload(
+      content,
+      content.length
+    );
+    console.log(
+      `Upload block blob ${blobName} successfully`,
+      uploadBlobResponse.requestId
+    );
+  } catch (error) {
     next(error);
   }
+};
+
+export const uploadFile = async (req, res, next) => {
+  try {
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+
+    const fileBuffer = req.file.buffer;
+    const fileType = req.file.originalname.split('.').at(-1);
+    const fileName = `${req.body.contribution_id}_${Date.now()}.${fileType}`;
+    const blockBlobClient = containerClient.getBlockBlobClient(fileName);
+    await blockBlobClient.uploadData(fileBuffer);
+
+    const fileDetails = new fileUploadModel({
+      contribution_id: req.body.contribution_id,
+      filepath: blockBlobClient.url,
+      fileSize: req.file.size,
+      fileName: fileName,
+      UploadDate: new Date(),
+    });
+    await blockBlobClient.uploadData(fileBuffer);
+    await fileDetails.save();
+
+    res.send('File uploaded successfully!');
+  } catch (err) {
+    next(err);
   }
-
-
-  export const uploadFile = async (req, res, next) => {
-    try {
-      const containerClient = blobServiceClient.getContainerClient(containerName);
-  
-      // Using upload.single("file") to handle single file upload with the field name "file"
-      upload.single("file")(req, res, async function (err) {
-        if (err) {
-          return next(err);
-        }
-  
-        const fileBuffer = req.file.buffer; 
-        const fileName = req.file.originalname  
-        const blockBlobClient = containerClient.getBlockBlobClient(fileName);
-        await blockBlobClient.uploadData(fileBuffer);
-
-
-        const fileDetails = new fileUploadModel({
-          contribution_id: req.body.contribution_id,
-          filepath: blockBlobClient.url,
-          fileSize: req.file.size,
-          fileName: fileName,
-          UploadDate: new Date()
-        });
-        await fileDetails.save();
-  
-        res.send("File uploaded successfully!");
-      });
-    } catch (err) {
-      next(err);
-    }
-  };
+};
--- apps/cms/src/routers/fileUpload.ts
+++ apps/cms/src/routers/fileUpload.ts
@@ -1,11 +1,23 @@
 import express from 'express';
-import { uploadFile , createBlob} from '../controllers/fileUpload.controller'
+import { uploadFile, createBlob } from '../controllers/fileUpload.controller';
 import { authorization, verifyToken } from '../middlewares/auth';
 import { RoleName } from '../models/role.model';
+import multer from 'multer';
 
 const router = express.Router();
+const upload = multer();
 
-router.get('/con/createBlob', createBlob )   
-router.post('/con/updateFile/', verifyToken, authorization([RoleName.MARKETING_MANAGER,RoleName.ADMIN,
-                                                             RoleName.MARKETING_COORDINATOR, RoleName.STUDENT]), uploadFile )   
+router.get('/con/createBlob', createBlob);
+router.post(
+  '/con/updateFile',
+  verifyToken,
+  authorization([
+    RoleName.MARKETING_MANAGER,
+    RoleName.ADMIN,
+    RoleName.MARKETING_COORDINATOR,
+    RoleName.STUDENT,
+  ]),
+  upload.single('file'),
+  uploadFile
+);
 export default router;
